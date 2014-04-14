2d8fd5a95e7de4984ef22f1a86d8e8a68b44023b
Valentin B. Saikov <saikov.vb@gmail.com>
2014-04-14 15:50:45 +0400

    Update
    Добавлена проверка на write.lock

--- mountfix.sh
+++ mountfix.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
 IP_WEB=192.168.1.108 # IP адрес шары
 PID_TOM="/var/run/tomcat6.pid"
 WRITELOCK="/var/lib/tomcat6/solr_home/data/index/write.lock"
 DIR_IN=/var/Motiw/scripts
 DIR_OUT="${IP_WEB}:/var/Motiw/scripts"
+
 filecount=`find $DIR_IN -type f | wc -l`
+date=`date "+%Y-%m-%d %k:%M"`
+
 if [ $filecount -eq 0 ]; then
 echo "Error. Not mount script NFS"
 #Монтируем /var/Motiw/scripts для tomcat
@@ -28,8 +32,17 @@ echo "Error. Not mount script NFS"
                         fi
                 fi
         fi
-echo "mount OK" >> /var/log/mountfix.log
+echo "${date} mount OK" >> /var/log/mountfix.log
 else
 #Если все смонтировано, ничего не делаем
-echo "mount OK" >> /var/log/mountfix.log && exit
+echo "${date} mount OK" >> /var/log/mountfix.log
+
+# Проверяем  дату write.lock. Если меньше чем сегодняшнее число, то удаляем
+DATE_LOCK=`ls -lt ${WRITELOCK} | awk '{print $7}'`
+DATE_TOMO=`date +%d`
+
+        if [[ ! $DATE_LOCK == $DATE_TOMO ]]; then
+                echo "${date} Зависший файл write.lock, удаляем и перезапускаем tomcat" >> /var/log/mountfix.log
+                rm $WRITELOCK
+        fi
 fi
